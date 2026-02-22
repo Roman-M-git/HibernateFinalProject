5378fffced8cb8bc2b5cd0b3efc020e6a3899d05
Roman-M-git <rmee.ok@gmail.com>
2026-02-22 18:32:11 +0200

    added classes

--- src/main/java/com/javarush/countryCache/Main.java
+++ src/main/java/com/javarush/countryCache/Main.java
@@ -1,4 +1,4 @@
-package com.javarush;
+package com.javarush.countryCache;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
